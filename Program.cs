6f5d5791dc3ee1a57dd91270c33d7a9e15b1c5f4
Thiago Campello <campellothiago@hotmail.com>
2024-01-05 11:46:19 -0300

    Desafio de projeto com POO. Concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("TESTE NOKIA");
+Nokia nokia = new Nokia("1234567890", "Tijolão", "871254", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.InformacoesSmartphone();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("TESTE IPHONE");
+Iphone iphone = new Iphone("0987654321", "X", "1827464", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+iphone.InformacoesSmartphone();
